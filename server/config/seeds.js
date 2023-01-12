92a8e92e2ba638955f58c1b876ea8fd5459a896e
David Herring <davidherring@Davids-MBP.home>
2023-01-11 19:58:12 -0600

    adding seeds to schemas

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,5 +1,86 @@
-const db = require('./connection');
-const { User, Waves, View } = require('../models');
+const db = require("./connection");
+const { User, Waves, View } = require("../models");
 
-console.log('Aura is Seeded');
+db.once("open", async () => {
+  const views = await View.insertMany([
+    {
+      viewText:
+        "Lorem Ipewrwersum is simply dummy text of the printing and typesetting industry",
+    },
+    {
+      viewText:
+        "Lorem Iewwerpsum is simply dummy text of the printing and typesetting industry",
+    },
+    {
+      viewText:
+        "Lorem Ipsuewrewm is simply dummy text of the printing and typesetting industry",
+    },
+    {
+      viewText:
+        "Lorem Ipserweum is simply dummy text of the printing and typesetting industry",
+    },
+    {
+      viewText:
+        "Lorem Ipwerewsum is simply dummy text of the printing and typesetting industry",
+    },
+  ]);
 
+  console.log("view is Seeded");
+
+  await User.create({
+    userName: "testUser",
+    firstName: "fname",
+    lastName: "lname",
+    email: "email@email.com",
+    password: "abc12345",
+  });
+  await User.create({
+    userName: "testUserTwo",
+    firstName: "fname",
+    lastName: "lname",
+    email: "emailTwo@email.com",
+    password: "abc12345",
+  });
+  await User.create({
+    userName: "testUserThree",
+    firstName: "fname",
+    lastName: "lname",
+    email: "emailThree@email.com",
+    password: "abc12345",
+  });
+  await User.create({
+    userName: "testUserFour",
+    firstName: "fname",
+    lastName: "lname",
+    email: "emailFour@email.com",
+    password: "abc12345",
+  });
+
+  console.log("users seeded");
+
+  const wave = await Waves.insertMany([
+    {
+      waveBody:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+    },
+    {
+      waveBody:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+    },
+    {
+      waveBody:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+    },
+    {
+      waveBody:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+    },
+    {
+      waveBody:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+    },
+  ]);
+  console.log("waves seeded..");
+
+  process.exit();
+});

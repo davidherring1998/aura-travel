664541634b2558526c665c392d94bbbd786bc9c1
Jeremiah Swearingen <jswearingen18@gmail.com>
2023-01-12 18:34:26 -0600

    updates

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,5 @@
 const mongoose = require ('mongoose');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://Aura-travlers:Travelers@arua-travelers.wxszlkl.mongodb.net/?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
+aur  useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 module.exports = mongoose.connection;

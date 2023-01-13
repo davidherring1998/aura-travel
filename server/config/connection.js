f6251c43a436e37f94b0ff741e0353a2c2959ea3
Jeremiah Swearingen <jswearingen18@gmail.com>
2023-01-12 18:35:30 -0600

    added auth

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/aura-travel",
-  {
+  mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://Aura-travlers:Travelers@arua-travelers.wxszlkl.mongodb.net/?retryWrites=true&w=majority', {
+    {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }

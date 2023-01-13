e5aa24ece0f702df22da6b865c3582c90d6b9ca4
Jeremiah Swearingen <jswearingen18@gmail.com>
2023-01-12 18:29:54 -0600

    added auth.js folder

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,9 @@
-const mongoose = require ('mongoose');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/aura-travel', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const mongoose = require("mongoose");
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/aura-travel",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 module.exports = mongoose.connection;

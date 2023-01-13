1306e9b640432d0fc2b491547a25cd580181b691
David Herring <davidherring@Davids-MBP.home>
2023-01-12 18:40:32 -0600

    fixing db

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,8 @@
+const mongoose = require('mongoose');
 
-const mongoose = require("mongoose");
-mongoose.connect(
-  mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://Aura-travlers:Travelers@arua-travelers.wxszlkl.mongodb.net/?retryWrites=true&w=majority', {
-    {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://Aura-travlers:Travelers@arua-travelers.wxszlkl.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 module.exports = mongoose.connection;
-

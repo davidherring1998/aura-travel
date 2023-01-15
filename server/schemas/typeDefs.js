91cda6180c1856dd4f72a39ae72c204ffcc92657
David Herring <davidherring@Davids-MBP.home>
2023-01-15 13:10:23 -0600

    adding typeDef

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,36 @@
 const { gql } = require('apollo-server-express');
+const typeDefs = gql`
+type User {
+    _id: ID
+    userName: String
+}
+
+type View {
+    viewText: String
+}
+
+type Waves {
+    waveBody: String 
+}
+
+type Auth {
+    token: ID
+    user: User
+  }
+
+type Query {
+    user: [User]
+    view: [View]
+    wave: [Waves]
+}
+
+
+type Mutation {
+    addUser(username: String!, firstName: String!, lastName: String!, email: String!, password: String!): Auth 
+    addView(viewText: String!): View
+    updateUser(username: String, firstName: String, lastName: String, email: String, password: String): User
+    updateView(_id: ID!): Waves
+    login(email: String!, password: String!): Auth
+}
+`
+ module.exports = typeDefs;

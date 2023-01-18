447c096abcc4a37e7b979dc92af9f1397581265a
Shelby Bridwell <109699848+shelbybridwell@users.noreply.github.com>
2023-01-17 20:04:55 -0600

    reducers

--- client/src/utils/actions.js
+++ client/src/utils/actions.js
@@ -1,9 +1,6 @@
 import { useReducer } from "react";
 
-export const LOGIN = "LOGIN";
-
-export const ADD_USER = "ADD_USER";
-export const ADD_VIEW = "ADD_VIEW";
-
+export const  ADD_WAVES = "ADD_WAVES";
+export const  UPDATE_WAVES = "UPDATE_WAVES";
 export const UPDATE_USER = "UPDATE_USER";
 export const UPDATE_VIEW = "UPDATE_VIEW";

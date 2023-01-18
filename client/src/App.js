84bc1e6bdf86010d7506851801e4f0ea494d379a
Jeremiah Swearingen <jswearingen18@gmail.com>
2023-01-18 14:20:48 -0600

    added some stylesheets

--- client/src/App.js
+++ client/src/App.js
@@ -1,50 +1,52 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import {
-    ApolloClient,
-    InMemoryCache,
-    ApolloProvider,
-    createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+// import {
+//     ApolloClient,
+//     InMemoryCache,
+//     ApolloProvider,
+//     createHttpLink,
+// } from '@apollo/client';
+// import { setContext } from '@apollo/client/link/context';
 
 import Home from './pages/Home';
 import Nav from './components/Nav';
+import Tools from './components/Tools'
 
-const httpLink = createHttpLink({
-    uri: '/graphql',
-    });
+// const httpLink = createHttpLink({
+//     uri: '/graphql',
+//     });
 
-const authLink = setContext((_, { headers }) => {
-    const token = localStorage.getItem('id_token');
-    return {
-        headers: {
-            ...headers,
-            authorization: token ? `Bearer ${token}` : '',
-        },
-    };
-});
-    const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
-});
+// const authLink = setContext((_, { headers }) => {
+//     const token = localStorage.getItem('id_token');
+//     return {
+//         headers: {
+//             ...headers,
+//             authorization: token ? `Bearer ${token}` : '',
+//         },
+//     };
+// });
+//     const client = new ApolloClient({
+//     link: authLink.concat(httpLink),
+//     cache: new InMemoryCache(),
+// });
 
 function App() {
     return (
-        <ApolloProvider client={client}>
-            <Router>
-               <div>
-                   <Nav />
-                   <Routes>
-                   <Route
-                   path='/'
-                    element={<Home />}
+        //<ApolloProvider client={client}>
+        <Router>
+            <div>
+                <Nav />
+                <Tools />
+                <Routes>
+                    <Route
+                        path='/'
+                        element={<Home />}
                     />
-        
-                   </Routes>
-               </div>
-           </Router>
-        </ApolloProvider>
+
+                </Routes>
+            </div>
+        </Router>
+        //</ApolloProvider>
     );
 }
 

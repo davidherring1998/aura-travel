77a47ec51108c978828a08a3a1a07c1f98bd37b5
Jeremiah Swearingen <jswearingen18@gmail.com>
2023-01-18 17:42:48 -0600

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import {
@@ -44,6 +45,58 @@ function App() {
       </Router>
     </ApolloProvider>
   );
+=======
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import {
+//     ApolloClient,
+//     InMemoryCache,
+//     ApolloProvider,
+//     createHttpLink,
+// } from '@apollo/client';
+// import { setContext } from '@apollo/client/link/context';
+
+import Home from './pages/Home';
+import Nav from './components/Nav';
+import Tools from './components/Tools'
+
+// const httpLink = createHttpLink({
+//     uri: '/graphql',
+//     });
+
+// const authLink = setContext((_, { headers }) => {
+//     const token = localStorage.getItem('id_token');
+//     return {
+//         headers: {
+//             ...headers,
+//             authorization: token ? `Bearer ${token}` : '',
+//         },
+//     };
+// });
+//     const client = new ApolloClient({
+//     link: authLink.concat(httpLink),
+//     cache: new InMemoryCache(),
+// });
+
+function App() {
+    return (
+        //<ApolloProvider client={client}>
+        <Router>
+            <div>
+                <Nav />
+                <Tools />
+                <Routes>
+                    <Route
+                        path='/'
+                        element={<Home />}
+                    />
+
+                </Routes>
+            </div>
+        </Router>
+        //</ApolloProvider>
+    );
+>>>>>>> 40f0b074bff39f5a4b448f2f738b0219d2099376
 }
 
 export default App;

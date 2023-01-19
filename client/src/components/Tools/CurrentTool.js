c16bb624ac2e08c9c21a3f5f33d5185ed2f81815
David Herring <davidherring@Davids-MBP.home>
2023-01-19 12:52:48 -0600

    adding maps

---
+++ client/src/components/Tools/CurrentTool.js
@@ -0,0 +1,30 @@
+import { React, useState } from "react";
+import Navigation from "./Maps/Maps";
+import Tools from "./index";
+import Weather from "./Weather/Weather";
+
+export default function ToolNavigation () {
+
+    const [currentPage, setCurrentPage] = useState("Tools");
+
+    const renderPage = () => {
+        if (currentPage === "Tools") {
+            return <Tools />
+        }
+        if (currentPage === "Navigation") {
+            return <Navigation />
+        }
+        if (currentPage === "Weather") {
+            return <Weather />
+        }
+    };
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    return (
+        <div>
+            <Tools currentPage={currentPage} handlePageChange={handlePageChange} />
+            {renderPage()}
+        </div>
+    )
+}

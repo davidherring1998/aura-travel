69a7fbab5ef680e15b89db4c7a0a06815c10987f
Jeremiah Swearingen <jswearingen18@gmail.com>
2023-01-19 14:07:57 -0600

    added Calendar and some styling to it

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "../../styles/Nav.css";
-import { AiOutlineGithub } from "react-icons/ai";
 
 // import Auth from "../../utils/auth";
 // import { Link } from "react-router-dom";
--- client/src/components/Tools/Weather/Weather.js
+++ client/src/components/Tools/Weather/Weather.js
@@ -1,33 +1,33 @@
 import React from "react";
-import "../styles/Weather.css";
+import "../../../styles/Weather.css";
 
 
-function Weather({ response, data}) {
-  
-const axios = require("axios");
+function Weather({ response, data }) {
 
-const options = {
-  method: 'GET',
-  url: 'https://open-weather13.p.rapidapi.com/city/fivedaysforcast/30.438/-89.1028',
-  headers: {
-    'X-RapidAPI-Key': '5ed129b857msh271befd335f319bp12cc22jsnd7eb320e9d54',
-    'X-RapidAPI-Host': 'open-weather13.p.rapidapi.com'
-  }
+  const axios = require("axios");
+
+  const options = {
+    method: 'GET',
+    url: 'https://open-weather13.p.rapidapi.com/city/fivedaysforcast/30.438/-89.1028',
+    headers: {
+      'X-RapidAPI-Key': '5ed129b857msh271befd335f319bp12cc22jsnd7eb320e9d54',
+      'X-RapidAPI-Host': 'open-weather13.p.rapidapi.com'
+    }
+  };
+
+  axios.request(options).then(function (response) {
+    console.log(response.data);
+  }).catch(function (error) {
+    console.error(error);
+  });
+
+
+  return (
+    <div className="weatherContainer">
+      {response.data}
+    </div>
+
+  )
 };
 
-axios.request(options).then(function (response) {
-  console.log(response.data);
-}).catch(function (error) {
-  console.error(error);
-});
-
-
-    return (
-        <div className="weatherContainer">
-        {response.data}      
-        </div>
-    
-    )
-  };
-  
 export default Weather; 

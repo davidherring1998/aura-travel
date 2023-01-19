24117d1e5a535893ef2cb454288afc9854585500
Jeremiah Swearingen <jswearingen18@gmail.com>
2023-01-18 18:10:37 -0600

    working again

--- client/src/App.js
+++ client/src/App.js
@@ -1,51 +1,48 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import {
-    ApolloClient,
-    InMemoryCache,
-    ApolloProvider,
-    createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import {
+//     ApolloClient,
+//     InMemoryCache,
+//     ApolloProvider,
+//     createHttpLink,
+// } from '@apollo/client';
+// import { setContext } from '@apollo/client/link/context';
 
-import Home from './pages/Home';
-import Nav from './components/Nav';
+import Home from "./pages/Home";
+import Nav from "./components/Nav";
+import Tools from "./components/Tools";
 
-const httpLink = createHttpLink({
-    uri: '/graphql',
-    });
+// const httpLink = createHttpLink({
+//     uri: '/graphql',
+//     });
 
-const authLink = setContext((_, { headers }) => {
-    const token = localStorage.getItem('id_token');
-    return {
-        headers: {
-            ...headers,
-            authorization: token ? `Bearer ${token}` : '',
-        },
-    };
-});
-    const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
-});
+// const authLink = setContext((_, { headers }) => {
+//     const token = localStorage.getItem('id_token');
+//     return {
+//         headers: {
+//             ...headers,
+//             authorization: token ? `Bearer ${token}` : '',
+//         },
+//     };
+// });
+//     const client = new ApolloClient({
+//     link: authLink.concat(httpLink),
+//     cache: new InMemoryCache(),
+// });
 
 function App() {
-    return (
-        <ApolloProvider client={client}>
-            <Router>
-                <div>
-                    <Nav />
-                    <Route>
-                        path="/"
-                        element={
-                            <Home />
-
-                        }
-                    </Route>
-                </div>
-            </Router>
-        </ApolloProvider>
-    );
+  return (
+    //<ApolloProvider client={client}>
+    <Router>
+      <div>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+    //</ApolloProvider>
+  );
 }
 
 export default App;

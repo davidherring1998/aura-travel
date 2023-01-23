abb8f6db0fb199d23ee9b11f83d019a409737505
Jeremiah Swearingen <jswearingen18@gmail.com>
2023-01-23 13:31:44 -0600

    ignore

--- client/src/App.js
+++ client/src/App.js
@@ -48,6 +48,17 @@ function App() {
                 <div className="calendarContainer">
                     <Calendar className="calendarComps" onChange={setDate} value={date} />
                 </div>
+                {date.length > 0 ? (
+                    <p>
+                        <span>Start:</span>{' '} {date[0].toDateString()}
+                        &nbsp; to &nbsp;
+                        <span>End:</span> {date[1].toDateString()}
+                    </p>
+                ) : (
+                    <p>
+                        <span>Default selected date:</span>{' '} {date.toDateString()}
+                    </p>
+                )}
                 <div className="textCenter">
                     Selected date: {date.toDateString()}
                 </div>

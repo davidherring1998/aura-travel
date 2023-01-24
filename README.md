0814b5db8905bf1784169423a6bd36862f842e96
Jeremiah Swearingen <jswearingen18@gmail.com>
2023-01-24 17:35:26 -0600

    readme

--- README.md
+++ README.md
@@ -1,70 +1,36 @@
-# Getting Started with Create React App
+# Aura-Travel
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## Description
+Aura Travel is a react app that has built in tools and apis that will allow you to budget and plan a trip  for your next trip.
 
-## Available Scripts
+## Github URL link
+N/A
 
-In the project directory, you can run:
+## Heroku URL link
 
-### `npm start`
+## Github Repository link
+https://github.com/davidherring1998/aura-travel
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+## Badges
+N/A
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+## Visuals
 
-### `npm test`
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+## Usage
+This is for planning and budgeting travel destinations
 
-### `npm run build`
+## Support
+Github
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+## Roadmap
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+## Contributions
+Shelby Bridwell, Lindsay Bae, David Herring, Jeremiah Swearingen
 
-### `npm run eject`
+## Authors and acknowledgment
+Shelby Bridwell, Lindsay Bae, David Herring, Jeremiah Swearingen
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+## License
+Through Vanderbilt University and Trilogy Education Services
--- client/src/components/Tools/Weather/Weather.js
+++ client/src/components/Tools/Weather/Weather.js
@@ -9,7 +9,7 @@ export default function WeatherCard() {
   // const [data, setData] = useState({});
   // const [test, setTest] =useState(1)
   // useEffect(() => {
-    
+
 
   //   fetch(`https://api.openweathermap.org/data/2.5/weather/?q=${city}&units=imperial&APPID=225569ec657be8e17ac296ced3863d8d`)
   //     .then(res => res.json())
@@ -32,10 +32,10 @@ export default function WeatherCard() {
   // }
 
   const [location, setLocation] = useState("Nashville")
-  const [data, setData] = useState(null)
-  const [temp, setTemp] = useState(null);
-  const [humidity, setHumidity] = useState(null);
-  const [wind, setWind] = useState(null);
+  const [data, setData] = useState("")
+  const [temp, setTemp] = useState("");
+  const [humidity, setHumidity] = useState("");
+  const [wind, setWind] = useState("");
 
   const handleLocationInput = (e) => {
     setLocation(e.target.value)
@@ -45,28 +45,28 @@ export default function WeatherCard() {
     e.preventDefault()
     fetchData()
   }
- 
-   function fetchData () {
-    
-      fetch(`https://api.openweathermap.org/data/2.5/weather/?q=${location}&units=imperial&APPID=225569ec657be8e17ac296ced3863d8d`)
+
+  function fetchData() {
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather/?q=${location}&units=imperial&APPID=225569ec657be8e17ac296ced3863d8d`)
       .then(res => res.json())
-      .then(res=> {
+      .then(res => {
         setData(res)
         console.log(res.main.temp)
       })
-      .then(res => setTemp(res))
+      .then(res => setTemp(res.main.temp))
 
-    }
-
+  }
 
   return (
     <div className="weatherContainer">
       <div> <div className="main">
         <div>
+          <div>{temp}{console.log()}</div>
         </div>
         <div className="header">Weather</div>
         <div className="input">
-          Enter City Name: 
+          Enter City Name:
           <form onSubmit={handleSubmit}><input
             type="text"
             name="city"
@@ -76,28 +76,53 @@ export default function WeatherCard() {
           /><button type="Submit" className="inputBtn">Submit</button>
           </form>
         </div>
-        <div className="flex">
-          {/* <p className="description">The currrent weather is {data.weather[0].main} in {data.name} on {moment().format('dddd')}, <span>{moment().format('LL')}</span>.</p> */}
-          <p>{temp}</p>
-        </div>
-        <div className="flex">
-          {/* <p className="temp">Temprature: <p className="data">{temp}&deg;F</p></p> */}
-          {/* <p>Temprature: {temp}</p> */}
-          {/* <p className="temp">Humidity: <p className="data">{data.main.humidity} %</p></p> */}
-          {/* <p className="temp">Wind Speed: <p className="data">{data.wind.speed} mph</p></p> */}
-        </div>
-        <div className="flex">
-          {/* <p className="sunrise-sunset">Sunrise: <p className="data">{new Date(data.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p></p> */}
-          {/* <p className="sunrise-sunset">Sunset: <p className="data">{new Date(data.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p></p> */}
-        </div>
+
       </div>
       </div>
 
     </div>
+  )
+  if (temp) {
+    return (
+      <div className="weatherContainer">
+        <div> <div className="main">
+          <div>
+          </div>
+          <div className="header">Weather</div>
+          <div className="input">
+            Enter City Name:
+            <form onSubmit={handleSubmit}><input
+              type="text"
+              name="city"
+              id="city"
+              placeholder="city"
+              onChange={handleLocationInput}
+            /><button type="Submit" className="inputBtn">Submit</button>
+            </form>
+          </div>
+          <div className="flex">
+            {/* <p className="description">The currrent weather is {data.weather[0].main} in {data.name} on {moment().format('dddd')}, <span>{moment().format('LL')}</span>.</p> */}
+            <p>{temp}</p>
+          </div>
+          <div className="flex">
+            {/* <p className="temp">Temprature: <p className="data">{temp}&deg;F</p></p> */}
+            {/* <p>Temprature: {temp}</p> */}
+            {/* <p className="temp">Humidity: <p className="data">{data.main.humidity} %</p></p> */}
+            {/* <p className="temp">Wind Speed: <p className="data">{data.wind.speed} mph</p></p> */}
+          </div>
+          <div className="flex">
+            {/* <p className="sunrise-sunset">Sunrise: <p className="data">{new Date(data.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p></p> */}
+            {/* <p className="sunrise-sunset">Sunset: <p className="data">{new Date(data.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p></p> */}
+          </div>
+        </div>
+        </div>
+
+      </div>
 
 
 
-  
-  );
+
+    );
+  }
 };
 
